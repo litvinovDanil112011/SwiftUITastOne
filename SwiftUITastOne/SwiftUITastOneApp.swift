df59831bce046b2ae534a1338236fae751c19766
litvinovDanil112011 <danil112011@mail.ru>
2022-03-24 23:07:22 +0300

    Initial Commit

---
+++ SwiftUITastOne/SwiftUITastOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITastOneApp.swift
+//  SwiftUITastOne
+//
+//  Created by Daniil Litvinov on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITastOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
